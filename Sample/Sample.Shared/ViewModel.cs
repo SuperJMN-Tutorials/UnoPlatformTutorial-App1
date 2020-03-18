b75c63b655dcec96b116479204203441ecc77f82
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-03-18 12:19:50 +0100

    Added properties and basic layout

--- Sample/Sample.Shared/ViewModel.cs
+++ Sample/Sample.Shared/ViewModel.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ReactiveUI;
+using Uno.Extensions;
 
 namespace Sample.Shared
 {
-    public class ViewModel
+    public class ViewModel : ReactiveObject
     {
+        private string firstName;
+        private string lastName;
+        
+        public string FirstName
+        {
+            get => firstName;
+            set => this.RaiseAndSetIfChanged(ref firstName, value);
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set => this.RaiseAndSetIfChanged(ref lastName, value);
+        }
     }
 }
