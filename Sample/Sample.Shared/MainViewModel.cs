65c70d2d0938fa196c859b12686a7782da02758d
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-03-18 16:09:05 +0100

    Introduced MainViewModel with bound Contacts

---
+++ Sample/Sample.Shared/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
+
+namespace Sample.Shared
+{
+    public class MainViewModel : ReactiveObject
+    {
+        public MainViewModel()
+        {
+            Contacts = new ObservableCollection<ContactViewModel>()
+            {
+                new ContactViewModel()
+                {
+                    FirstName= "Pablo",
+                    LastName = "Nieto",
+                },
+                new ContactViewModel()
+                {
+                    FirstName= "Rafa",
+                    LastName = "Nadal",
+                },
+                new ContactViewModel()
+                {
+                    FirstName= "Robert",
+                    LastName = "Martin",
+                },
+            };
+        }
+
+        public ObservableCollection<ContactViewModel> Contacts { get; }
+    }
+}
